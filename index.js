ac737ecc561115914eb50b8ce6e21931d5f308e2
Rahul Nandrajog <2016.rahul.nandrajog@ves.ac.in>
2019-03-18 22:39:48 +0530

    code working successfully !!!!

--- index.js
+++ index.js
@@ -17,124 +17,33 @@ app.get('/' , function (req , res ) {
 
 
 app.post('/compilecode' , function (req , res ) {
-    
-	var code = req.body.code;	
-	var input = req.body.input;
+	  var code = req.body.code;	
+	  var input = req.body.input;
     var inputRadio = req.body.inputRadio;
     var lang = req.body.lang;
-    if((lang === "C") || (lang === "C++"))
-    {        
-        if(inputRadio === "true")
-        {    
-        	var envData = { OS : "windows" , cmd : "g++"};	   	
-        	compiler.compileCPPWithInput(envData , code ,input , function (data) {
-        		if(data.error)
-        		{
-        			res.send(data.error);    		
-        		}
-        		else
-        		{
-        			res.send(data.output);
-        		}
-        	});
-	   }
-	   else
-	   {
-	   	
-	   	var envData = { OS : "windows" , cmd : "g++"};	   
-        	compiler.compileCPP(envData , code , function (data) {
-        	if(data.error)
-        	{
-        		res.send(data.error);
-        	}    	
-        	else
-        	{
-        		res.send(data.output);
-        	}
-    
-            });
-	   }
-    }
-    if(lang === "Java")
-    {
-        if(inputRadio === "true")
-        {
-            var envData = { OS : "windows" };     
-            console.log(code);
-            compiler.compileJavaWithInput( envData , code , function(data){
-                res.send(data);
-            });
-        }
-        else
-        {
-            var envData = { OS : "windows" };     
-            console.log(code);
-            compiler.compileJavaWithInput( envData , code , input ,  function(data){
-                res.send(data);
-            });
-
-        }
-
-    }
     if( lang === "Python")
     {
         if(inputRadio === "true")
         {
             var envData = { OS : "windows"};
             compiler.compilePythonWithInput(envData , code , input , function(data){
-                res.send(data);
+                res.send(data["output"]);
             });            
         }
         else
         {
             var envData = { OS : "windows"};
             compiler.compilePython(envData , code , function(data){
-                res.send(data);
+                res.send(data["output"]);
             });
         }
-    }
-    if( lang === "CS")
-    {
-        if(inputRadio === "true")
-        {
-            var envData = { OS : "windows"};
-            compiler.compileCSWithInput(envData , code , input , function(data){
-                res.send(data);
-            });            
-        }
-        else
-        {
-            var envData = { OS : "windows"};
-            compiler.compileCS(envData , code , function(data){
-                res.send(data);
-            });
-        }
-
-    }
-    if( lang === "VB")
-    {
-        if(inputRadio === "true")
-        {
-            var envData = { OS : "windows"};
-            compiler.compileVBWithInput(envData , code , input , function(data){
-                res.send(data);
-            });            
-        }
-        else
-        {
-            var envData = { OS : "windows"};
-            compiler.compileVB(envData , code , function(data){
-                res.send(data);
-            });
-        }
-
     }
 
 });
 
 app.get('/fullStat' , function(req , res ){
     compiler.fullStat(function(data){
-        res.send(data);
+        res.send(data["output"]);
     });
 });
 
